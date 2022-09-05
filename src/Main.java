ed4014e66a42506a74a677aa1f6d0511269ef9df
lordpank <lordpank@gmail.com>
2022-09-05 12:55:27 +0300

    added solution

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long minors = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.printf(String.valueOf(minors));
+
+        List<String> recruits = persons.stream()
+                .filter(person -> person.getSex().equals(Sex.MAN))
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getAge() < 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+
+        System.out.println(recruits);
+
+        List<Person> ableBodiedPopulation = persons.stream()
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> (person.getSex().equals(Sex.WOMAN) & (person.getAge() < 60)) ||
+                        (person.getSex().equals(Sex.MAN) & (person.getAge() < 65)))
+                .sorted((Comparator.comparing(Person::getFamily)))
+                .collect(Collectors.toList());
+
+        System.out.println(ableBodiedPopulation);
+
+    }
+}
